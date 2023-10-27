80f7fb32893fd2c423455a5ae4b033f27ff39ed2
TiredFox07 <peyton.a.murphy@gmail.com>
2023-10-26 22:45:44 -0400

    removed useless lines

--- lua/tiredfox07/packer.lua
+++ lua/tiredfox07/packer.lua
@@ -15,9 +15,6 @@ return require('packer').startup(function(use)
   use({
 	  'rose-pine/neovim',
 	  as = 'rose-pine',
-	  config = function()
-        vim.cmd('colorscheme rose-pine')
-	  end
   })
 
   use("folke/tokyonight.nvim")
