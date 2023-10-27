a4c99b7ad5a835378c7e5b01d84bfd513a9c873c
TiredFox07 <peyton.a.murphy@gmail.com>
2023-10-26 22:34:25 -0400

    changed theme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,6 +1,10 @@
 
+require("tokyonight").setup({
+    style = "moon",
+})
+
 function ColorMyPencils(color)
-	colors = color or "rose-pine"
+	color = color or "tokyonight"
 	vim.cmd.colorscheme(color)
 
 	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
